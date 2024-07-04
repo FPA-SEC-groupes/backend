2a3f448193e6421b63047cda4ac1d215beff26ac
HelloWay2 <contact.helloway@gmail.com>
2024-07-04 03:05:24 +0100

    rebuild 6

--- src/main/java/com/HelloWay/HelloWay/config/FirebaseConfig.java
+++ src/main/java/com/HelloWay/HelloWay/config/FirebaseConfig.java
@@ -2,6 +2,7 @@ package com.HelloWay.HelloWay.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
@@ -9,17 +10,21 @@ import com.google.firebase.FirebaseOptions;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
     @Bean
-    public FirebaseApp initializeFirebase() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/helloway-fd5cd-firebase-adminsdk-fq74d-368296d91e.json");
+public FirebaseApp initializeFirebase() throws IOException {
+    // Load the Firebase config file from the classpath
+    ClassPathResource resource = new ClassPathResource("helloway-fd5cd-firebase-adminsdk-fq74d-368296d91e.json");
+    InputStream serviceAccount = resource.getInputStream();
 
-        FirebaseOptions options = FirebaseOptions.builder()
-            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-            .build();
+    // Initialize Firebase
+    FirebaseOptions options = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+        .build();
 
-        return FirebaseApp.initializeApp(options);
-    }
+    return FirebaseApp.initializeApp(options);
+}
 }
