970375f5cd04a039dda4631ee47f216385b0deac
moutiasaad <moutiasaad481@gmail.com>
2025-05-26 12:39:14 +0100

    Exclude SystemMetricsAutoConfiguration from SpringBootApplication and refactor CORS configuration in HelloWay class

--- src/main/java/com/HelloWay/HelloWay/HelloWay.java
+++ src/main/java/com/HelloWay/HelloWay/HelloWay.java
@@ -2,8 +2,8 @@ package com.HelloWay.HelloWay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,43 +13,44 @@ import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 
-@SpringBootApplication
-@EnableScheduling 
+@SpringBootApplication(exclude = {SystemMetricsAutoConfiguration.class})
+@EnableScheduling
 public class HelloWay {
-	public String PORT =System.getenv("PORT");
-	public static void main(String[] args) {
-		SpringApplication.run(HelloWay.class, args);
-	}
+    public String PORT = System.getenv("PORT");
 
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWay.class, args);
+    }
 
-	@Bean	
-	public CorsFilter corsFilter() {
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://192.168.1.193","http://localhost:3001", "http://localhost:3002"));
-		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
-		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-		return new CorsFilter(urlBasedCorsConfigurationSource);
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "http://192.168.1.193",
+                "http://localhost:3001",
+                "http://localhost:3002"
+        ));
+        corsConfiguration.setAllowedHeaders(Arrays.asList(
+                "Origin", "Access-Control-Allow-Origin", "Content-Type",
+                "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers"
+        ));
+        corsConfiguration.setExposedHeaders(Arrays.asList(
+                "Origin", "Content-Type", "Accept", "Authorization",
+                "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"
+        ));
+        corsConfiguration.setAllowedMethods(Arrays.asList(
+                "GET", "POST", "PUT", "DELETE", "OPTIONS"
+        ));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
 
-
-
-	}
-
-//		@EventListener(ApplicationReadyEvent.class)
-//		public void sendMail(){
-//		 senderService.sendEmail("alaeddinemansouri373@gmail.com","this is subject","this is body from my  first mailing application");
-//		}
-
-
-	@Bean
-	public  static PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
